ed02221b2a1dfe522d32b3403d674da1b46d4f71
Haibo Wu <wuhaibo23@gmail.com>
2016-04-11 13:59:14 +0200

    add unitTest mocking data from html file

--- angularUnitTest/app/js/services.js
+++ angularUnitTest/app/js/services.js
@@ -19,3 +19,28 @@ unitTestApp.factory('GetUserNumberService',
        return deferred.promise;  
     }
 );
+
+
+
+unitTestApp.factory('readDataFromHtml',    
+    function($http,$q) {
+       var deferred = $q.defer();
+       
+       //http 服务请求
+       $http({method: 'GET', url: '/auth.py'}).then(
+           function(response){
+              var htmlContent = response.data;
+ 
+              var userName = $(htmlContent).find("div#username").text();
+              deferred.resolve(userName); 
+           },
+           function (response) {
+              deferred.reject(response); 
+           }
+       );
+       
+       //返回http 服务请求的promise
+       return deferred.promise;  
+    }
+);
+
--- angularUnitTest/test/unit/servicesSpec.js
+++ angularUnitTest/test/unit/servicesSpec.js
@@ -35,6 +35,39 @@ describe('Test GetUserNumberService', function() {
         //测试判断userNum是否为2
         expect(userNum).toEqual(2);     
     }));
+    
+    it('test mock read html from file', 
+        inject(function($injector) {
+            
+        //模拟返回数据   
+        var fixture = jasmine.getFixtures();
+        fixture.fixturesPath = 'base';
+        fixture.load('test/mock/data.html');
+        //get full html
+        var htmlPage = $('html')[0].outerHTML;
+        var $httpBackend = $injector.get('$httpBackend');
+        $httpBackend.whenGET('/auth.py').respond(htmlPage);
+        
+         // 通过injector得到service    
+         var readDataFromHtmlService = $injector.get('readDataFromHtml');          
+         var promise = readDataFromHtmlService;
+         var userName;
+         promise.then(function(data){
+             userName = data;
+         });
+         
+         //强迫httpBackend返回数据
+         $httpBackend.flush();  
+         
+         //通过injector得到$rootScope      
+        var $rootScope = $injector.get('$rootScope');    
+        //强迫传递到当前作用域
+        $rootScope.$apply();     
+        
+        expect(userName).toContain('test');   
+        
+    }));
+            
 
 });
     
